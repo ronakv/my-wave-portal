b4013690f0dd97009792715f85f27139088567ec
Ronak Vimadalal <ronakvimadalal@live.com>
2022-10-29 19:59:26 -0700

    Added Goerli deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
 
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      // This value will be replaced on runtime
+      url: process.env.GOERLI_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
